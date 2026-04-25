714d841a4fce9ac8b1520f9e275cc081bb6f9a78
Poom <pakpoomtk1@gmail.com>
2026-04-26 02:44:14 +0700

    Enhance signup process with ID card upload and consent options

--- jobs_app/adapters.py
+++ jobs_app/adapters.py
@@ -5,9 +5,14 @@ from .models import UserProfile
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
+        profile, _ = UserProfile.objects.get_or_create(user=user)
         avatar_b64 = request.POST.get('avatar_b64', '')
         if avatar_b64:
-            profile, _ = UserProfile.objects.get_or_create(user=user)
             profile.avatar_b64 = avatar_b64
-            profile.save()
+        id_card = request.FILES.get('id_card_image')
+        if id_card:
+            profile.id_card_image = id_card
+        profile.no_criminal_record = 'no_criminal_record' in request.POST
+        profile.consent_id_upload = 'consent_id_upload' in request.POST
+        profile.save()
         return user
