4c198facb06245d6bcd978651b3ef0dc9ee36d90
Poom <pakpoomtk1@gmail.com>
2026-04-26 02:51:34 +0700

    Add base64 ID card upload functionality to signup process

--- jobs_app/adapters.py
+++ jobs_app/adapters.py
@@ -1,3 +1,5 @@
+import base64
+from django.core.files.base import ContentFile
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from .models import UserProfile
 
@@ -9,9 +11,15 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
         avatar_b64 = request.POST.get('avatar_b64', '')
         if avatar_b64:
             profile.avatar_b64 = avatar_b64
-        id_card = request.FILES.get('id_card_image')
-        if id_card:
-            profile.id_card_image = id_card
+        id_card_b64 = request.POST.get('id_card_b64', '')
+        if id_card_b64 and ';base64,' in id_card_b64:
+            try:
+                header, data = id_card_b64.split(';base64,', 1)
+                profile.id_card_image = ContentFile(
+                    base64.b64decode(data), name=f'id_card_{user.id}.jpg'
+                )
+            except Exception:
+                pass
         profile.no_criminal_record = 'no_criminal_record' in request.POST
         profile.consent_id_upload = 'consent_id_upload' in request.POST
         profile.save()
